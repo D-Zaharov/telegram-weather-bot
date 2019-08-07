5c007df3a75c8d3a90007bdd9d64948ef5b339d7
D-Zaharov <Zdaniil1512@gmail.com>
2019-08-07 16:29:08 +0300

    ключ от API теперь передается функции в качестве параметра

--- weather_func.py
+++ weather_func.py
@@ -1,8 +1,8 @@
 ﻿import pyowm
 
-def get_tempereture(place_name):
+def get_tempereture(api_key, place_name):
     try:
-        owm = pyowm.OWM(' ') #здесь ключ от API
+        owm = pyowm.OWM(api_key)
         observation = owm.weather_at_place(place_name)
         weather = observation.get_weather()
         tempereture = weather.get_temperature('celsius').get('temp')
