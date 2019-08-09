3f5780d90ee9a8cdf8ba8316a2822c03676e65fe
Mkw <starbirdyt@gmail.com>
2019-08-09 15:14:23 +0300

    Bot created

--- bot.py
+++ bot.py
@@ -1,15 +1,17 @@
 ﻿import telebot
 from telebot import apihelper
 
-apihelper.proxy = {'https':'socks5://Naatkok:jGgTzR7kvzIFpdReXcrd22V15MfHtDOKdSUbWXgH@vpnzone.technology:10580'}
+apihelper.proxy = {'https':'socks5h://Naatkok:jGgTzR7kvzIFpdReXcrd22V15MfHtDOKdSUbWXgH@vpnzone.technology:10580'}
 
-bot = telebot.TeleBot(' '); #здесь ключ от робота
+import keys
+
+bot = telebot.TeleBot(keys.bot);
 
 import weather_func
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, я могу подсказать тебе прогноз погоды на сегодня. Введи город, где хочешь узнать прогноз.')
+    bot.send_message(message.chat.id, 'Привет! Я могу дать тебе информацию о погоде в городе, который ты выберешь. Напиши сначала тип данных, который хочешь получить и затем название города. Пока доступны три варианта: Скорость ветра, Температура, Погодные условия.')
 
 @bot.message_handler(content_types=['text'])
 
@@ -18,11 +20,25 @@ def send_text(message):
         bot.send_message(message.chat.id, 'Привет! В каком городе хочешь узнать погоду?')
     elif message.text.lower() == 'пока':
         bot.send_message(message.chat.id, 'До новых встреч')
-    else:
-        weather = weather_func.get_tempereture(message.text)
+    elif message.text.lower()[:11] == 'температура':
+        weather = weather_func.get_tempereture(keys.api, message.text)
         if weather == None:
-            bot.send_message(message.chat.id, 'Извини, ничего не могу найти. Введи только город в котором ты хочешь узнать погоду в именительном падеже.')
+            bot.send_message(message.chat.id, 'Извини, ничего не могу найти.')
         else:
-            bot.send_message(message.chat.id, 'погода в твоем городе ' + str(weather) + 'градусов')
+            bot.send_message(message.chat.id, 'Температура в выбранном городе ' + str(weather) + 'градусов')
+    elif message.text.lower()[:14] == 'скорость ветра':
+        weather = weather_func.get_wind(keys.api, message.text)
+        if weather == None:
+            bot.send_message(message.chat.id, 'Извини, ничего не могу найти.')
+        else:
+            bot.send_message(message.chat.id, 'Скорость ветра в выбранном городе ' + str(weather) + 'метров в секунду')
+    elif message.text.lower()[:16] == 'погодные условия':
+        weather = weather_func.get_details(keys.api, message.text)
+        if weather == None:
+            bot.send_message(message.chat.id, 'Извини, ничего не могу найти.')
+        else:
+            bot.send_message(message.chat.id, 'Погодные условия в выбранном городе: ' + str(weather))
+    else:
+        bot.send_message(message.chat.id, 'Не соблюдён формат!')
 
 bot.polling(none_stop=True, interval=0)
--- weather_func.py
+++ weather_func.py
@@ -1,11 +1,29 @@
 ﻿import pyowm
 
-def get_tempereture(place_name):
+def get_tempereture(api_key, place_name):
     try:
-        owm = pyowm.OWM(' ') #здесь ключ от API
-        observation = owm.weather_at_place(place_name)
+        owm = pyowm.OWM(api_key)
+        observation = owm.weather_at_place(place_name[12:])
         weather = observation.get_weather()
         tempereture = weather.get_temperature('celsius').get('temp')
+    except:
+        tempereture = None
+    return(tempereture)
+def get_wind(api_key, place_name):
+    try:
+        owm = pyowm.OWM(api_key)
+        observation = owm.weather_at_place(place_name[15:])
+        weather = observation.get_weather()
+        tempereture = weather.get_wind().get('speed')
+    except:
+        tempereture = None
+    return(tempereture)
+def get_details(api_key, place_name):
+    try:
+        owm = pyowm.OWM(api_key)
+        observation = owm.weather_at_place(place_name[17:])
+        weather = observation.get_weather()
+        tempereture = weather.get_detailed_status()
     except:
         tempereture = None
     return(tempereture)
